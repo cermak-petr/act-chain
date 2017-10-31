1b0608afe85a944336aa73cbcdc9c6d1cf4ef5fc
Petr Čermák <30902390+cermak-petr@users.noreply.github.com>
2017-11-01 00:54:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const Apify = require('apify');
 const _ = require('underscore');
+const extend = require('extend');
 const Promise = require('bluebird');
 
 function getOutput(output){
-    return (output.output ? output.output.body : false) || output.output || output;
+    return output ? (output.body || output) : undefined;
 }
 
 async function postWebhook(url, execIds){
@@ -16,26 +17,60 @@ async function postWebhook(url, execIds){
     await request(options);
 }
 
+async function runAct(actId, input, opts){
+    const data = extend({
+        actId: actId, 
+        body: JSON.stringify(input)
+    }, opts);
+    if(!data.contentType){data.contentType = 'application/json';}
+    return await Apify.client.acts.runAct(data);
+}
+
+async function waitForRun(actId, runId){
+    let run = null;
+    const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+    while(run === null || run.status === 'RUNNING'){
+        run = await Apify.client.acts.getRun({actId, runId});
+        await wait(1000);
+    }
+    return await Apify.client.keyValueStores.getRecord({storeId: run.defaultKeyValueStoreId, key: 'OUTPUT'});
+}
+
 Apify.main(async () => {
     const input = await Apify.getValue('INPUT');
     const state = (await Apify.getValue('STATE')) || {index: 0, output: input};
     const data = input.data ? (typeof input.data === 'string' ? JSON.parse(input.data) : input.data) : input;
+    
     if(!data.acts){
         return console.log('missing "acts" attribute in INPUT');
     }
-    while(state.index < input.acts.length){
+    
+    async function waitForLatest(){
         const act = input.acts[state.index];
-        const actInput = act.input || getOutput(state.output);
-        console.log('starting act: ' + act.actId);
-        console.log('input: ');
-        console.dir(actInput);
-        state.output = await Apify.call(act.actId, actInput, act.opts);
+        state.output = await waitForRun(act.actId, state.latest.id);
+        state.latest = null;
         state.index++;
         await Apify.setValue('STATE', state);
         console.log('act finished: ' + act.actId);
     }
-    await Apify.setValue('OUTPUT', getOutput(output));
+    
+    if(state.latest){waitForLatest();}
+    
+    while(state.index < input.acts.length){
+        const act = input.acts[state.index];
+        const actInput = act.input || getOutput(state.output);
+        if(!actInput){
+            return console.log('no input for the next act, stopping execution');
+        }
+        console.log('starting act: ' + act.actId + '\ninput: ');
+        console.dir(actInput);
+        state.latest = await runAct(act.actId, actInput, act.opts);
+        await Apify.setValue('STATE', state);
+        await waitForLatest();
+    }
+    
+    await Apify.setValue('OUTPUT', getOutput(state.output));
     if(data.finalWebhook){
-        await postWebhook(input.finalWebhook, output);
+        await postWebhook(input.finalWebhook, state.output);
     }
 });
